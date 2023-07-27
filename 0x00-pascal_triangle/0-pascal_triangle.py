32771cc2f4f7d12f3dd1f70925820c7f6074faad
BLACKMouha <mohamedsenghor05@gmail.com>
2023-07-27 11:27:06 +0000

    Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,42 +1,41 @@
 #!/usr/bin/python3
 ''' pascal_triangle module '''
-from math import factorial
 
+'''
+For a row x, the number of elements is x + 1
+For a row x, elements at at column 0 and x are set to 1
+All other elements are got with the sum of the two numbers just above it.
+This means for a row x, an integer at column y is got by the sum of element at
+[x-1, y-1] + [x-1, y]
+At row 0, we have a list of 1 element composed by 1
+At row 1, we have a list of 2 elements composed by 1, 1
+And so on.
+So for a row x, the number of elements is x + 1
 
-def binomial_coef(x, y):
-    '''
-    Computes binomial coefficient of two integers
+x is between 0 and n (n excluded)
+y is between 0 and x (x included)
 
-    Args:
-        x: integer, must be greater than or equal to y
-        y: integer, must be less than or equal to x
-    Return: an integer, the binomial coefficient on success.
-            On failure, -1 is returned
-    '''
-    if x < 0 or y < 0 or x < y:
-        return -1
-    return int(factorial(x) / (factorial(y) * factorial(x - y)))
-
+A sublist is a sequence of numbers got witht the sum of two elements above it.
+Each sublist is appended to main list, and in turn the main is returned by the
+function.
+'''
 
 def pascal_triangle(n):
     '''
-    Builds a Pascal's Triangle of lenght @n:
+    Build a Pascal Triangle of leng @n
 
     Args:
-        n: an integer representing the number of rows in the triangle
-    Return: a list of list of integers like a triangular array of numbers.
+        n: integer, the number of rows in the Triangle
+    Return: return a list of list of numbers like a triangular array of numbers
     '''
-
-    if n <= 0:
-        return []
-
     t, s = [], []
-    b = 0
 
     for i in range(n):
         s = []
         for j in range(i + 1):
-            b = binomial_coef(i, j)
-            s.append(b)
+            if j == 0 or j == i:
+                s.append(1)
+            else:
+                s.append(t[i-1][j-1] + t[i-1][j])
         t.append(s)
     return t
