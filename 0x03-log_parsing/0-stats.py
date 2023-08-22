1936ad71006def38b622ebd4fbd92fed87048bda
BLACKMouha <mohamedsenghor05@gmail.com>
2023-08-22 11:19:50 +0000

    Reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,29 +1,45 @@
 #!/usr/bin/python3
-
-'''0-stats script
-Reads stdin line by line and computes metrics'''
-
+'''
+0-stats script
+Reads stdin line by line and computes metrics
+'''
 
 import re
 import sys
 
+if __name__ == '__main__':
+    try:
+        regex = re.compile(r'\d+.\d+.\d+.\d+ - \[\d+-\d+\d+-\d+ \d+:\d+:\d+\.\d+\] \"GET \/projects\/260 HTTP\/1\.1\" (\d+) (\d+)')
+        sum_file_size = 0
+        code_occurencies = {
+            200: 0,
+            301: 0,
+            400: 0,
+            401: 0,
+            403: 0,
+            404: 0,
+            405: 0,
+            500: 0
+        }
+        while True:
+            for i in range(10):
+                line = input()
+                code = int(regex.match(line).group(1))
+                code_occurencies[code] += 1
+                file_size = regex.match(line).group(2)
+                sum_file_size += int(file_size)
 
-try:
-    regex = re.compile(r'\d+.\d+.\d+.\d+ - \[\d+-\d+\d+-\d+ \d+:\d+:\d+\.\d+\] \"GET \/projects\/260 HTTP\/1\.1\" (\d+) (\d+)')
-    sum_file_size = 0
-    code_occurencies = {
-        200: 0,
-        301: 0,
-        400: 0,
-        401: 0,
-        403: 0,
-        404: 0,
-        405: 0,
-        500: 0
-    }
-    while True:
+            print('File size: {}'.format(sum_file_size))
+
+            for code_occurency in code_occurencies:
+                if (code_occurencies[code_occurency] != 0):
+                    print(
+                        f'{code_occurency}: {code_occurencies[code_occurency]}'
+                    )
+
+            sys.stdin.flush()
+    except KeyboardInterrupt as k:
         for i in range(10):
-            line = input()
             code = int(regex.match(line).group(1))
             code_occurencies[code] += 1
             file_size = regex.match(line).group(2)
@@ -36,19 +52,5 @@ try:
                 print(f'{code_occurency}: {code_occurencies[code_occurency]}')
 
         sys.stdin.flush()
-except KeyboardInterrupt as k:
-    for i in range(10):
-        code = int(regex.match(line).group(1))
-        code_occurencies[code] += 1
-        file_size = regex.match(line).group(2)
-        sum_file_size += int(file_size)
-
-    print('File size: {}'.format(sum_file_size))
-
-    for code_occurency in code_occurencies:
-        if (code_occurencies[code_occurency] != 0):
-            print(f'{code_occurency}: {code_occurencies[code_occurency]}')
-
-    sys.stdin.flush()
-except ValueError as v:
-    print('Status code or File size is not a number :(')
+    except ValueError as v:
+        print('Status code or File size is not a number :(')
