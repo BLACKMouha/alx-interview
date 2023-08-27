9f20361f9091c8e7cfe975b3f8cd8d7e0e35b698
BLACKMouha <mohamedsenghor05@gmail.com>
2023-08-27 12:08:45 +0000

    Reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,54 +6,48 @@ Reads stdin line by line and computes metrics
 '''
 
 import sys
+import re
+
 
 if __name__ == '__main__':
+    nlr = 0 # number of lines read
+    sum_size = 0 # sum of file sizes
+    codes = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
+    }
+    regex = (r'\d+.\d+.\d+.\d+ - \[\d+-\d+\d+-\d+ \d+:\d+:\d+\.\d+\] '
+             r'\"GET \/projects\/260 HTTP\/1\.1\" (\d+) (\d+)')
+
     try:
-        sum_file_size = 0
-        codes = {
-            200: 0,
-            301: 0,
-            400: 0,
-            401: 0,
-            403: 0,
-            404: 0,
-            405: 0,
-            500: 0
-        }
-        while True:
-            for i in range(10):
-                line = input().rstrip().split(' ')
-                code = int(line[-2])
+        for line in sys.stdin:
+            if re.search(regex, line.strip()):
+                nlr += 1
+                line = line.strip().split()
+                code, file_size = line[-2], line[-1]
+                try:
+                    sum_size += int(file_size)
+                except ValueError as v:
+                    pass
                 codes[code] += 1
-                file_size = line[-1]
-                sum_file_size += int(file_size)
-
-            print('File size: {}'.format(sum_file_size))
-
-            for c in codes:
-                if (codes[c] != 0):
-                    print(f'{c}: {codes[c]}')
-            sys.stdin.flush()
-    except KeyboardInterrupt as k:
-        try:
-            for i in range(10):
-                line = input().rstrip().split(' ')
-                code = int(line[-2])
-                codes[code] += 1
-                file_size = line[-1]
-                sum_file_size += int(file_size)
-            print('File size: {}'.format(sum_file_size))
-            for c in codes:
-                if (codes[c] != 0):
-                    print(f'{c}: {codes[c]}')
-        except EOFError as eof:
-            print(f'File size: {sum_file_size}')
-            for c in codes:
-                if (codes[c] != 0):
-                    print(f'{c}: {codes[c]}')
-        finally:
-            sys.stdin.flush()
-    except ValueError as v:
-        print('Status code or File size is not a number :(')
+                if nlr == 10:
+                    nlr = 0
+                    print(f'File size: {sum_size}')
+                    for c in codes:
+                        if codes[c] > 0:
+                            print(f'{c}: {codes[c]}')
+                sys.stdin.flush()
+    except (KeyboardInterrupt, EOFError) as e:
+        print(f'File size: {sum_size}')
+        for c in codes:
+            if codes[c] > 0:
+                print(f'{c}: {codes[c]}')
+        sys.stdin.flush()
     finally:
         sys.stdin.flush()
