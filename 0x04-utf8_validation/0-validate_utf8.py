487208f9ef84477fa4e05258029268bf8799b6e4
BLACKMouha <mohamedsenghor05@gmail.com>
2023-08-30 16:26:18 +0000

    validUTF8 method determines if a given data set represents a valid UTF-8 encoding

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,8 +7,21 @@ def validUTF8(data):
     Arg:
         data: a list of integers, each one corresponding to a character
     Return:
-        True when all the characters are ASCII else False.'''
-    try:
-        return bytearray(data).isascii()
-    except Exception as e:
-        return False
+        True when the data set follows the UTF8 rules otherwise False'''
+    n_bytes = 0
+    for num in data:
+        bits = bin(num).lstrip('0b').zfill(8)
+        if n_bytes == 0:
+            for bit in bits:
+                if bit == '0':
+                    break
+                n_bytes += 1
+            if n_bytes == 0:
+                continue
+            if n_bytes == 1 or n_bytes > 4:
+                return False
+        else:
+            if not (bits[0] == '1' and bits[1] == '0'):
+                return False
+        n_bytes -= 1
+    return n_bytes == 0
