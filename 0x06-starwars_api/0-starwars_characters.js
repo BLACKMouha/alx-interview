9b37ef4ad34ee38193016e09bf97f0b8204d4075
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-21 16:02:54 +0000

    Displaying all characters name of a Star Wars movie

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-/* jshint esversion: 6 */
+/* jshint esversion: 8 */
 
 /**
  * Print all characters of a Star Wars movie
  * There is only one argument passed to the program and is the movie ID
  */
+
 if (process.argv.length === 3) {
   const request = require('request');
   const id = process.argv[2];
@@ -14,14 +15,15 @@ if (process.argv.length === 3) {
     if (e) {
       throw (e);
     } else {
-      for (const p of JSON.parse(b).characters) {
-        request.get(p, function (x, t, y) {
-          if (x) {
-            throw (x);
-          } else {
-            console.log(JSON.parse(y).name);
-          }
+      const characters_urls = JSON.parse(b).characters;
+      if (characters_urls instanceof Array) {
+        characters_urls.map(character_url => {
+          request.get(character_url, function (err, res, bod) {
+            console.log(JSON.parse(bod).name);
+          });
         });
+      } else {
+        console.log([]);
       }
     }
   });
