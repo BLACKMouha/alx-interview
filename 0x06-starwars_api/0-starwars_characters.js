3a546deec26a4cc9e9aa3c5aa3abdff40a8cbc2c
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-22 08:44:45 +0000

    Displaying Star Wars Characters of a specific movie based on the ID of the movie

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,26 +5,31 @@
  * Print all characters of a Star Wars movie
  * There is only one argument passed to the program and is the movie ID
  */
+const request = require('request');
 
-if (process.argv.length === 3) {
-  const request = require('request');
-  const id = process.argv[2];
-  const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+const id = process.argv[2] || 1;
+const movieUrl = 'https://swapi-api.alx-tools.com/api/films/' + id;
 
-  const characters_urls = request.get(url, (err, res, body) => {
+function getMovieCharacters (charactersUrls, index = 0) {
+  if (index >= charactersUrls.length) {
+    return;
+  }
+  const characterUrl = charactersUrls[index];
+  request(characterUrl, (err, res, body) => {
     if (err) {
-      console.error(err);
+      throw (err);
     } else {
-      const characters_urls = JSON.parse(body).characters;
-      characters_urls.forEach((Character_url) => {
-        request.get(Character_url, (e, r, b) => {
-          if (e) {
-            console.log(e);
-          } else {
-            console.log(JSON.parse(b).name);
-          }
-        });
-      });
+      console.log(JSON.parse(body).name);
+      getMovieCharacters(charactersUrls, index + 1);
     }
   });
 }
+
+request(movieUrl, (err, res, body) => {
+  if (err) {
+    throw (err);
+  } else {
+    const charactersUrls = (JSON.parse(body).characters);
+    getMovieCharacters(charactersUrls, 0);
+  }
+});
