b7245abb5c3e839496c5d67b3688c8ed1b92427f
BLACKMouha <mohamedsenghor05@gmail.com>
2023-10-19 14:42:01 +0000

    Rotate a 2D Array

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''0-rotate_2d_matrix module'''
+
+def rotate_2d_matrix(matrix):
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i):
+            tmp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = tmp
+
+    for i in range(n):
+        for j in range(int(n / 2)):
+            matrix[i] = list(reversed(matrix[i]))
