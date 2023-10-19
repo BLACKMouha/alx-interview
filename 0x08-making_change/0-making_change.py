1422697bd4ebef15ca661278529284b28b79409d
BLACKMouha <mohamedsenghor05@gmail.com>
2023-10-19 08:38:21 +0000

    Making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''0-making_change module'''
+
+
+def makeChange(coins: list, total: int):
+    '''Making change'''
+    if total == 0:
+        return 0
+    coins = sorted(coins, reverse=True)
+    numCoins = 0
+
+    for coin in coins:
+        while total >= coin:
+            numCoins += 1
+            total -= coin
+    return numCoins if total == 0 else -1
